18e01cd0b8208aafceff110511a5eab834d3d4c4
menasehk13 <53655445+menasehk13@users.noreply.github.com>
2020-07-28 00:39:57 +0300

    viewcreated

--- app/src/main/java/com/example/hightechstudents/CourseFragment.java
+++ app/src/main/java/com/example/hightechstudents/CourseFragment.java
@@ -2,6 +2,8 @@ package com.example.hightechstudents;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -18,5 +20,11 @@ public class CourseFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_course, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //textview=view.findbyid();
     }
 }
