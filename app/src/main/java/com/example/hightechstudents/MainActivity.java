3078625e5ec3cb70c959d43c5a4fd24c2a0783a1
menasehk13 <53655445+menasehk13@users.noreply.github.com>
2020-08-19 23:46:12 +0300

    FullyUpdated

--- app/src/main/java/com/example/hightechstudents/MainActivity.java
+++ app/src/main/java/com/example/hightechstudents/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -23,99 +25,51 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class MainActivity extends AppCompatActivity {
-    Toolbar toolbar;
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
-    TextView username,studentid,departemnt;
-    FirebaseFirestore firebaseFirestore;
-    FirebaseAuth auth;
-    String userid;
+public class MainActivity extends AppCompatActivity  {
+    CardView gradereport,course,setting,semister_grade,classroom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-       firebaseFirestore=FirebaseFirestore.getInstance();
-       auth=FirebaseAuth.getInstance();
-       userid=auth.getCurrentUser().getUid();
-       DocumentReference documentReference=firebaseFirestore.collection("studentPro").document(userid);
-         documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-             @Override
-             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-              if (task.isSuccessful()){
-                  String user,id,depart;
-                  DocumentSnapshot documentSnapshot= task.getResult();
-                  if (documentSnapshot.exists()){
-                      Log.d("TAG","Document of students"+documentSnapshot.getData());
-                      user = (String) documentSnapshot.get("firstName");
-                      id=(String) documentSnapshot.get("ID");
-                      depart=(String) documentSnapshot.get("Department");
-                      username.setText(user);
-                      studentid.setText(id);
-                      departemnt.setText(depart);
-                  }else{
-                      Log.d("TAG","no such Document");
-                  }
-
-              }else{
-                  Log.d("TAG","get failed with "+task.getException());
-              }
-
-             }
-
-         });
-
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView=findViewById(R.id.navView);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-        View header=navigationView.getHeaderView(0);
-        departemnt=header.findViewById(R.id.student_view_departemnt);
-        studentid=header.findViewById(R.id.studentviewid);
-        SelectedItem(R.id.homeNav);
-         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-               @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                   SelectedItem(item.getItemId());
-             return true;
+        gradereport=findViewById(R.id.gradereport);
+        course=findViewById(R.id.course);
+        setting=findViewById(R.id.setting);
+        semister_grade=findViewById(R.id.semister_grade);
+        classroom=findViewById(R.id.classroom);
+        gradereport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,GradeView.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
             }
-         });
+        });
+        course.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        semister_grade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        classroom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
-      public void SelectedItem(int Items){
-          Fragment fragment = null;
-        switch (Items){
-            case R.id.homeNav:
-                fragment=new HomeFragment();
-                break;
-            case R.id.gradeNav:
-                fragment=new GradeFragment();
-                break;
-            case R.id.courseNav:
-                fragment=new CourseFragment();
-                break;
-            case R.id.settingNav:
-                fragment = new SettingFragment();
-                break;
-            default:
-        }
-          FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.frame,fragment);
-        ft.commit();
-        drawerLayout.closeDrawer(GravityCompat.START);
-      }
-    @Override
-    public void onBackPressed() {
-        if(drawerLayout.isDrawerOpen(GravityCompat.START))
-        {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        }
-        else
-        {
-            super.onBackPressed();
-        }
-    }
+
+
 }
