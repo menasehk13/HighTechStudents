47b5ca8f0b2e34f0540ee8f1440824afedd6144a
menasehk13 <53655445+menasehk13@users.noreply.github.com>
2020-08-21 15:55:14 +0300

    Fixed The issue

--- app/src/main/java/com/example/hightechstudents/ClassRoom.java
+++ app/src/main/java/com/example/hightechstudents/ClassRoom.java
@@ -10,5 +10,6 @@ public class ClassRoom extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_room);
+
     }
 }
--- app/src/main/java/com/example/hightechstudents/LoginActivity.java
+++ app/src/main/java/com/example/hightechstudents/LoginActivity.java
@@ -25,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onStart();
         if (auth.getCurrentUser()!=null){
             Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
         }
     }
