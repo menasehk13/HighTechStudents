a5fec25b285fe931309eed9c7e1a95159a499ee0
menasehk13 <53655445+menasehk13@users.noreply.github.com>
2020-08-24 11:51:10 +0300

    new update

--- app/src/main/java/com/example/hightechstudents/ProfileView.java
+++ app/src/main/java/com/example/hightechstudents/ProfileView.java
@@ -2,11 +2,15 @@ package com.example.hightechstudents;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -26,12 +30,13 @@ import java.util.Calendar;
 
 import static java.lang.String.*;
 
-public class ProfileView extends AppCompatActivity {
+public class ProfileView extends AppCompatActivity implements View.OnClickListener {
     TextView proDep,proSec,proId,proName,proPhone,proRegYear;
     DocumentReference proReference;
     FirebaseFirestore proFirestore;
     FirebaseAuth firebaseAuth;
-    String userId;;
+    String userId;
+    Button backbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +47,9 @@ public class ProfileView extends AppCompatActivity {
         proId=findViewById(R.id.userstudentid);
         proPhone=findViewById(R.id.userstudentphone);
         proSec=findViewById(R.id.sectioncount);
+        backbutton=findViewById(R.id.backbutton);
         firebaseAuth=FirebaseAuth.getInstance();
+        backbutton.setOnClickListener(this);
         userId=firebaseAuth.getCurrentUser().getUid();
         proFirestore=FirebaseFirestore.getInstance();
         proReference=proFirestore.collection("StudentPro").document(userId);
@@ -63,5 +70,43 @@ public class ProfileView extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent=new Intent(ProfileView.this,MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent=new Intent(ProfileView.this,MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
+        startActivity(intent);
+    }
+
+    public void Logout_Clicked(View view) {
+        final AlertDialog.Builder alertDialog= new AlertDialog.Builder(this);
+        alertDialog.setMessage("Are you Sure You Want to LogOut");
+        alertDialog.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                firebaseAuth.signOut();
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                finish();
+            }
+        });
+        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                return;
+            }
+        });
+       AlertDialog dialog=alertDialog.create();
+       dialog.show();
     }
 }
